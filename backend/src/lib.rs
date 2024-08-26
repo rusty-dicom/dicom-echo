bb1f2bfdb4f6d94db1e0fd0b40c6f3d0e41b3ec6
Bryant Finney <bryant.finney@outlook.com>
2024-08-25 22:58:57 -0400

    wip: begin definition of the backend API
    Signed-off-by: Bryant Finney <bryant.finney@outlook.com>

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,9 +1,52 @@
+/// Implement the backend module in Rust.
+// TODO: use dicom_ul::ClientAssociation;
 use pyo3::prelude::*;
 
+/// Parameters for the C-STORE request
+#[pyclass(module = "backend", get_all)]
+#[derive(Debug)]
+struct Request {
+    address: String,
+    called_ae_title: String,
+    calling_ae_title: String,
+    message_id: u16,
+}
+
+#[pymethods]
+impl Request {
+    #[new]
+    fn new(
+        address: String,
+        called_ae_title: String,
+        calling_ae_title: String,
+        message_id: u16,
+    ) -> Self {
+        Request {
+            address,
+            called_ae_title,
+            calling_ae_title,
+            message_id,
+        }
+    }
+
+    fn __repr__(&self) -> String {
+        format!("{:?}", self)
+    }
+
+    fn __str__(&self) -> String {
+        format!(
+            r#"Request(address="{}", called_ae_title="{}", calling_ae_title="{}", message_id="{}")"#,
+            self.address, self.called_ae_title, self.calling_ae_title, self.message_id
+        )
+    }
+
+    fn send(&self) -> u8 {
+        0
+    }
+}
+
 /// Formats the sum of two numbers as string.
-#[pyfunction]
-#[pyo3(name = "do_sum")]
-#[pyo3(text_signature = "(a: c_uint, b: c_uint) -> str")]
+#[pyfunction(name = "do_sum", text_signature = "(a: c_uint, b: c_uint) -> str")]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
@@ -12,5 +55,6 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 #[pymodule]
 fn backend(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_class::<Request>()?;
     Ok(())
 }
--- src/dicom_echo/backend.pyi
+++ src/dicom_echo/backend.pyi
@@ -1,3 +1,14 @@
+class Request:
+    """Send `C-ECHO` requests to a DICOM server."""
+
+    address: str
+    called_ae_title: str
+    calling_ae_title: str
+    message_id: int
+
+    def __init__(self, address: str, called_ae_title: str, calling_ae_title: str, message_id: int) -> None: ...
+    def send(self) -> int: ...
+
 def do_sum(a: int, b: int) -> str:
     """Sum two numbers and return the result as a string.
 
